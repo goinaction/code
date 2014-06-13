c6a1249560967ac5bc548b2fdefce45b44f5b46b
William Kennedy <bill@ardanstudios.com>
2014-06-13 19:11:35 -0400

    With empty struct don't use a pointer

--- chapter2/sample/search/default.go
+++ chapter2/sample/search/default.go
@@ -5,11 +5,11 @@ type defaultMatcher struct{}
 
 // init registers the default matcher with the program.
 func init() {
-	var matcher *defaultMatcher
+	var matcher defaultMatcher
 	Register("default", matcher)
 }
 
 // Search implements the behavior for the default matcher.
-func (m *defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
+func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
 	return nil, nil
 }
