5ceb3afbb271f86d0d75767acaa05eec9febdd68
William Kennedy <bill@ardanstudios.com>
2014-09-10 18:25:40 -0400

    Saving code for chapter 7 logging

---
+++ chapter7/listing03/listing03.go
@@ -0,0 +1,22 @@
+// This sample program demonstrates how to use the base log package.
+package main
+
+import (
+	"log"
+)
+
+func init() {
+	log.SetPrefix("PREFIX: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
+
+func main() {
+	// Println writes to the standard logger.
+	log.Println("message")
+
+	// Fatalln is Println() followed by a call to os.Exit(1).
+	log.Fatalln("fatal message")
+
+	// Panicln is Println() followed by a call to panic().
+	log.Panicln("panic message")
+}
---
+++ chapter7/listing11/listing11.go
@@ -0,0 +1,47 @@
+// This sample program demonstrates how to create customized loggers.
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	Trace   *log.Logger // Just about anything
+	Info    *log.Logger // Important information
+	Warning *log.Logger // Be concerned
+	Error   *log.Logger // Critical problem
+)
+
+func init() {
+	file, err := os.OpenFile("errors.txt",
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open error log file:", err)
+	}
+
+	Trace = log.New(ioutil.Discard,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(os.Stdout,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(os.Stdout,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(io.MultiWriter(file, os.Stderr),
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func main() {
+	Trace.Println("I have something standard to say")
+	Info.Println("Special Information")
+	Warning.Println("There is something you need to know about")
+	Error.Println("Something has failed")
+}
