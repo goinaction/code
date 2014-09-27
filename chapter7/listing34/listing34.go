3341d8dd7b2d10def9e3e8c144b3cb9457f9bb46
William Kennedy <bill@ardanstudios.com>
2014-09-27 09:36:55 -0400

    code for io package

---
+++ chapter7/listing34/listing34.go
@@ -0,0 +1,25 @@
+// Sample program to show how different functions from the
+// standard library use the io.Writer interface.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+// main is the entry point for the application.
+func main() {
+	// Create a Buffer value and write a string to the buffer.
+	// Using the io.Writer implementation for Buffer.
+	var b bytes.Buffer
+	b.Write([]byte("Hello "))
+
+	// Use Fprintf to write the string to the Buffer.
+	// Using the io.Writer implementation for Buffer.
+	fmt.Fprintf(&b, "World!")
+
+	// Write the content of the Buffer to the stdout device.
+	// Using the io.Writer implementation for os.File.
+	b.WriteTo(os.Stdout)
+}
