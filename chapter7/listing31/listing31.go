a15eba590412ed68a2e15aa465c97a34bacb3ae6
William Kennedy <bill@ardanstudios.com>
2014-10-06 11:18:30 -0400

    new example

---
+++ chapter7/listing31/listing31.go
@@ -0,0 +1,36 @@
+// This sample program demonstrates how to decode a JSON string.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// JSON contains a sample string to unmarshal.
+var JSON = `{
+	"name": "Gopher",
+	"title": "programmer",
+	"contact": {
+		"home": "415.333.3333",
+		"cell": "415.555.5555"
+	}
+}`
+
+func main() {
+	// Unmarshal the JSON string into our map variable.
+	var c map[string]interface{}
+	err := json.Unmarshal([]byte(JSON), &c)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+
+	data, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+
+	fmt.Println(string(data))
+}
