de0ae4327fae39f5a58ab948c41f2ac8ef666a6c
gbbr <ga@stripetree.co.uk>
2014-10-24 20:40:20 +0100

    Pool readability proposal

--- chapter7/patterns/pool/main/main.go
+++ chapter7/patterns/pool/main/main.go
@@ -15,45 +15,35 @@ import (
 )
 
 const (
-	// maxGoroutines is the number of routines to use.
-	maxGoroutines = 25
-
-	// pooledResources is the number of resources to pool.
-	pooledResources = 2
+	maxGoroutines   = 25 // the number of routines to use.
+	pooledResources = 2  // number of resources in the pool
 )
 
 var (
-	// connectionID maintains a counter.
-	connectionID int32
-
-	// wg is used to wait for the program to finish.
-	wg sync.WaitGroup
+	connectionID int32          // counter
+	wg           sync.WaitGroup // waits for program to finish
 )
 
 // dbConnection simulates a resource to share.
-type dbConnection struct {
-	ID int32
-}
+type dbConnection struct{ ID int32 }
 
 // Close implements the interface for the pool package.
 // Close performs any resource release management.
 func (dbConn *dbConnection) Close() {
 	fmt.Println("Close: Connection", dbConn.ID)
-	return
 }
 
 // createConnection is a factory method called by the pool
 // framework when new connections are needed.
 func createConnection() (pool.Resource, error) {
 	id := atomic.AddInt32(&connectionID, 1)
-
 	fmt.Println("Create: New Connection", id)
+
 	return &dbConnection{id}, nil
 }
 
 // main is the entry point for all Go programs.
 func main() {
-	// Add a count for each goroutine.
 	wg.Add(maxGoroutines)
 
 	// Create the buffered channel to hold
@@ -64,8 +54,12 @@ func main() {
 	}
 
 	// Perform queries using a connection from the pool.
-	for query := 0; query < maxGoroutines; query++ {
-		go performQueries(query, p)
+	for q := 0; q < maxGoroutines; q++ {
+		go func() {
+			performQueries(q, p)
+			wg.Done()
+		}()
+
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 	}
 
@@ -78,10 +72,7 @@ func main() {
 }
 
 // performQueries tests the resource pool of connections.
-func performQueries(query int, p *pool.Pool) {
-	// Schedule the call to Done to tell main we are done.
-	defer wg.Done()
-
+func performQueries(query int, p pool.Interface) {
 	// Acquire a connection from the pool.
 	conn, err := p.Acquire()
 	if err != nil {
@@ -89,10 +80,10 @@ func performQueries(query int, p *pool.Pool) {
 		return
 	}
 
-	// Release the connection back to the pool.
-	defer p.Release(conn)
-
 	// Wait to simulate a query response.
 	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 	fmt.Printf("Query: QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
+
+	// Release the connection back to the pool.
+	p.Release(conn)
 }
