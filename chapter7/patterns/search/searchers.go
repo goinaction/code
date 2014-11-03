53e02ee32df67de89aa9ee71712e992ddd3355e1
gbbr <ga@stripetree.co.uk>
2014-11-03 18:46:19 +0000

    Renamed to shorter things

--- chapter7/patterns/search/searchers.go
+++ chapter7/patterns/search/searchers.go
@@ -20,25 +20,25 @@ type google struct{}
 
 // Search implements the Searcher interface. It performs a search
 // against Google.
-func (g google) Search(term string, resChan chan<- []Result) {
+func (g google) Search(term string, results chan<- []Result) {
 	log.Printf("Google : Search : Started : search term[%s]\n", term)
 
 	// Slice for the results.
-	var results []Result
+	var r []Result
 
 	// Simulate an amount of time for the search.
 	time.Sleep(time.Millisecond * time.Duration(rand.Int63n(900)))
 
 	// Simulate a result for the search.
-	results = append(results, Result{
+	r = append(r, Result{
 		Engine:      "Google",
 		Title:       "The Go Programming Language",
 		Description: "The Go Programming Language",
 		Link:        "https://golang.org/",
 	})
 
-	log.Printf("Google : Search : Completed : Found[%d]\n", len(results))
-	resChan <- results
+	log.Printf("Google : Search : Completed : Found[%d]\n", len(r))
+	results <- r
 }
 
 // Bing provides support for Bing searches.
@@ -46,25 +46,25 @@ type bing struct{}
 
 // Search implements the Searcher interface. It performs a search
 // against Bing.
-func (b bing) Search(term string, resChan chan<- []Result) {
+func (b bing) Search(term string, results chan<- []Result) {
 	log.Printf("Bing : Search : Started : search term [%s]\n", term)
 
 	// Slice for the results.
-	var results []Result
+	var r []Result
 
 	// Simulate an amount of time for the search.
 	time.Sleep(time.Millisecond * time.Duration(rand.Int63n(900)))
 
 	// Simulate a result for the search.
-	results = append(results, Result{
+	r = append(r, Result{
 		Engine:      "Bing",
 		Title:       "A Tour of Go",
 		Description: "Welcome to a tour of the Go programming language.",
 		Link:        "http://tour.golang.org/",
 	})
 
-	log.Printf("Bing : Search : Completed : Found[%d]\n", len(results))
-	resChan <- results
+	log.Printf("Bing : Search : Completed : Found[%d]\n", len(r))
+	results <- r
 }
 
 // Yahoo provides support for Yahoo searches.
