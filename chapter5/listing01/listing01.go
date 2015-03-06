89f5cd88e3b58314d5f2c9217e79d036a2069e2a
William Kennedy <bill@ardanstudios.com>
2015-03-05 22:31:48 -0500

    Updated code example

---
+++ chapter5/listing01/listing01.go
@@ -0,0 +1,48 @@
+// Sample program to show how to declare methods and how the Go
+// compiler supports them.
+package main
+
+import (
+	"fmt"
+)
+
+// user defines a user in the program.
+type user struct {
+	name  string
+	email string
+}
+
+// notify implements a method with a value receiver.
+func (u user) notify() {
+	fmt.Printf("User: Sending User Email To %s<%s>\n",
+		u.name,
+		u.email)
+}
+
+// changeEmail implements a method with a pointer receiver.
+func (u *user) changeEmail(email string) {
+	u.email = email
+}
+
+// main is the entry point for the application.
+func main() {
+	// Values of type user can be used to call methods
+	// declared with a value receiver.
+	bill := user{"Bill", "bill@email.com"}
+	bill.notify()
+
+	// Pointers of type user can also be used to call methods
+	// declared with a value receiver.
+	lisa := &user{"Lisa", "lisa@email.com"}
+	lisa.notify()
+
+	// Values of type user can be used to call methods
+	// declared with a pointer receiver.
+	bill.changeEmail("bill@gmail.com")
+	bill.notify()
+
+	// Pointers of type user can be used to call methods
+	// declared with a pointer receiver.
+	lisa.changeEmail("lisa@gmail.com")
+	lisa.notify()
+}
