bd6cd813f3238f7f40bdfaa0bf946769e8101a23
William Kennedy <bill@ardanstudios.com>
2015-04-03 19:38:04 -0400

    new code listings for chapter 5

---
+++ chapter5/listing48/listing48.go
@@ -0,0 +1,45 @@
+// Sample program to show how to embed a type into another type and
+// the relationship between the inner and outer type.
+package main
+
+import (
+	"fmt"
+)
+
+// user defines a user in the program.
+type user struct {
+	name  string
+	email string
+}
+
+// notify implements a method that can be called via
+// a value of type user.
+func (u *user) notify() {
+	fmt.Printf("Sending user email To %s<%s>\n",
+		u.name,
+		u.email)
+}
+
+// admin represents an admin user with privileges.
+type admin struct {
+	user  // Embedded Type
+	level string
+}
+
+// main is the entry point for the application.
+func main() {
+	// Create an admin user.
+	ad := admin{
+		user: user{
+			name:  "john smith",
+			email: "john@yahoo.com",
+		},
+		level: "super",
+	}
+
+	// We can access the inner type's method directly.
+	ad.user.notify()
+
+	// The inner type's method is promoted.
+	ad.notify()
+}
